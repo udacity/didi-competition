225abbc5663917b6487c40a98977a0b95bb41ccd
Ian Zhang <ianzhang1986@gmail.com>
2017-04-05 21:51:26 -0700

    [Update] Capture vehicle front/rear gps/rtk topic prefix

--- tracklets/python/bag_topic_def.py
+++ tracklets/python/bag_topic_def.py
@@ -7,9 +7,9 @@ CAMERA_TOPICS = [SINGLE_CAMERA_TOPIC]
 OLD_GPS_TOPIC = "/gps/fix"
 OLD_RTK_TOPIC = "/gps/rtkfix"
 
-CAP_REAR_GPS_TOPIC = "/capture_vehicle/rear/gps/fix"
-CAP_REAR_RTK_TOPIC = "/capture_vehicle/rear/gps/rtkfix"
-CAP_FRONT_GPS_TOPIC = "/capture_vehicle/front/gps/fix"
-CAP_FRONT_RTK_TOPIC = "/capture_vehicle/front/gps/rtkfix"
+CAP_REAR_GPS_TOPIC = "/objects/capture_vehicle/rear/gps/fix"
+CAP_REAR_RTK_TOPIC = "/objects/capture_vehicle/rear/gps/rtkfix"
+CAP_FRONT_GPS_TOPIC = "/objects/capture_vehicle/front/gps/fix"
+CAP_FRONT_RTK_TOPIC = "/objects/capture_vehicle/front/gps/rtkfix"
 
 OBJECTS_TOPIC_ROOT = "/objects"
