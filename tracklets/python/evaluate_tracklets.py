9606c9d36998653e0f565b41375e4cd0894c3d7a
rwightman <rwightman@gmail.com>
2017-06-02 18:50:02 -0700

    Add pedestrian support to evaluation script. * Pedestrian supported via cylinder volume intersection (not yaw sensitive) * Separate intersection and metric calculations * Add ability to switch volume based metrics, add optional dice score option * Volume calculation method (geometry to use) argument is now an override, otherwise determiend by object type

--- tracklets/python/evaluate_tracklets.py
+++ tracklets/python/evaluate_tracklets.py
@@ -10,10 +10,11 @@ import argparse
 import os
 import sys
 import yaml
+import math
 
 from parse_tracklet import *
 
-VOLUME_METHODS = ['box', 'sphere']
+VOLUME_METHODS = ['box', 'cylinder', 'sphere']
 
 
 def lwh_to_box(l, w, h):
@@ -25,15 +26,14 @@ def lwh_to_box(l, w, h):
     return box
 
 
-def iou_bbox_with_yaw(vol_a, box_a, vol_b, box_b):
+def intersect_bbox_with_yaw(box_a, box_b):
     """
-    A simplified calculation of 3d bounding box intersection
-    over union. It is assumed that the bounding box is only rotated
+    A simplified calculation of 3d bounding box intersection.
+    It is assumed that the bounding box is only rotated
     around Z axis (yaw) from an axis-aligned box.
 
-    :param vol_a, vol_b: precalculated obstacle volumes for comparison
     :param box_a, box_b: obstacle bounding boxes for comparison
-    :return: iou float, intersection volume float
+    :return: intersection volume (float)
     """
     # height (Z) overlap
     min_h_a = np.min(box_a[2])
@@ -44,27 +44,32 @@ def iou_bbox_with_yaw(vol_a, box_a, vol_b, box_b):
     min_of_max = np.min([max_h_a, max_h_b])
     z_intersection = np.max([0, min_of_max - max_of_min])
     if z_intersection == 0:
-        return 0., 0.
+        return 0.
 
     # oriented XY overlap
     xy_poly_a = Polygon(zip(*box_a[0:2, 0:4]))
     xy_poly_b = Polygon(zip(*box_b[0:2, 0:4]))
     xy_intersection = xy_poly_a.intersection(xy_poly_b).area
     if xy_intersection == 0:
-        return 0., 0.
+        return 0.
 
-    intersection = z_intersection * xy_intersection
-    union = vol_a + vol_b - intersection
-    iou = intersection / union
-    return iou, intersection
+    return z_intersection * xy_intersection
 
 
-def iou_sphere(vol_a, sphere_a, vol_b, sphere_b):
+def intersect_sphere(sphere_a, sphere_b):
+    """
+    3d sphere intersection.
+
+    :param sphere_a, sphere_b: spheres for comparison
+        packed as x,y,z,r, xyz points to center of sphere
+    :return: intersection volume (float)
+    """
+    assert len(sphere_a) == len(sphere_b) == 4
     dist = np.linalg.norm(sphere_a[0:3] - sphere_b[0:3])
     r_a, r_b = sphere_a[3], sphere_b[3]
     if dist >= r_a + r_b:
         # spheres do not overlap in any way
-        return 0., 0.
+        return 0.
     elif dist <= abs(r_a - r_b):
         # one sphere fully inside the other (includes coincident)
         # take volume of smaller sphere as intersection
@@ -75,9 +80,59 @@ def iou_sphere(vol_a, sphere_a, vol_b, sphere_b):
         intersection = (r_a + r_b - dist)**2
         intersection *= (dist**2 + 2*dist*(r_a + r_b) - 3*(r_a - r_b)**2)
         intersection *= np.pi / (12*dist)
-    union = vol_a + vol_b - intersection
-    iou = intersection / union
-    return iou, intersection
+    return intersection
+
+
+def intersect_cylinder(cyl_a, cyl_b):
+    """
+    3d cylinder intersection.
+
+    :param cyl_a, cyl_b: cylinders for comparison
+        packed as x,y,z,r,h, xyz points to center of axis aligned (h along z) cylinder
+    :return: intersection volume (float)
+    """
+    assert len(cyl_a) == len(cyl_b) == 5
+    xyz_a, xyz_b = cyl_a[0:3], cyl_b[0:3]
+    r_a, r_b = cyl_a[3], cyl_b[3]
+    h_a, h_b = cyl_a[4], cyl_b[4]
+
+    # height (Z) overlap
+    zh_a = [xyz_a[2] + h_a/2, xyz_a[2] - h_a/2]
+    zh_b = [xyz_b[2] + h_b/2, xyz_b[2] - h_b/2]
+    max_of_min = np.max([zh_a[1], zh_b[1]])
+    min_of_max = np.min([zh_a[0], zh_b[0]])
+    z_intersection = np.max([0, min_of_max - max_of_min])
+    print('zi', z_intersection)
+    if z_intersection == 0:
+        return 0.
+
+    dist = np.linalg.norm(cyl_a[0:2] - cyl_b[0:2])
+    print('dist', dist)
+    if dist >= r_a + r_b:
+        # cylinders do not overlap in any way
+        return 0.
+    elif dist <= abs(r_a - r_b):
+        # one cylinder fully inside the other (includes coincident)
+        # take volume of smaller sphere as intersection
+        intersection = np.pi * min(r_a, r_b)**2 * z_intersection
+        print('rdiff', abs(r_a - r_b))
+    else:
+        def _lens(r, ro, d):
+            td = (d**2 + r**2 - ro**2)/(2*d)
+            return r**2 * math.acos(td/r) - td * math.sqrt(r**2 - td**2)
+        circle_intersection = _lens(r_a, r_b, dist) + _lens(r_b, r_a, dist)
+        print('ci', circle_intersection)
+        intersection = circle_intersection * z_intersection
+    return intersection
+
+
+def iou(vol_a, vol_b, vol_intersect):
+    union = vol_a + vol_b - vol_intersect
+    return vol_intersect / union if union else 0.
+
+
+def dice(vol_a, vol_b, vol_intersect):
+    return 2 * vol_intersect / (vol_a + vol_b) if (vol_a + vol_b) else 0.
 
 
 class Obs(object):
@@ -109,29 +164,52 @@ class Obs(object):
         # sphere passed as 4 element vector with radius as last element
         return np.append(self.position, r)
 
+    def get_cylinder(self):
+        r = max(self.w, self.l)/2
+        # cylinder passed as 5 element vector with radius and height as last elements
+        return np.append(self.position, [r, self.h])
+
     def get_vol_sphere(self):
         r = max(self.h, self.w, self.l)/2
         return 4/3. * np.pi * r**3
+
+    def get_vol_cylinder(self):
+        r = max(self.w, self.l)/2
+        return np.pi * r**2 * self.h
 
     def get_vol_box(self):
         return self.h * self.w * self.l
 
     def get_vol(self, method='box'):
-        return self.get_vol_sphere() if method == 'sphere' else self.get_vol_box()
-
-    def intersection(self, other, method='box'):
         if method == 'sphere':
-            iou_val, intersection_vol = iou_sphere(
-                self.get_vol_sphere(), self.get_sphere(),
-                other.get_vol_sphere(), other.get_sphere())
+            return self.get_vol_sphere()
+        elif method == 'cylinder':
+            return self.get_vol_cylinder()
+        return self.get_vol_box()
+
+    def intersection_metric(self, other, method='box', metric_fn=iou):
+        if method == 'sphere':
+            intersection_vol = intersect_sphere(self.get_sphere(), other.get_sphere())
+            metric_val = metric_fn(self.get_vol_sphere(), other.get_vol_sphere(), intersection_vol)
+        elif method == 'cylinder':
+            intersection_vol = intersect_cylinder(self.get_cylinder(), other.get_cylinder())
+            metric_val = metric_fn(self.get_vol_cylinder(), other.get_vol_cylinder(), intersection_vol)
         else:
-            iou_val, intersection_vol = iou_bbox_with_yaw(
-                self.get_vol_box(), self.get_bbox(),
-                other.get_vol_box(), other.get_bbox())
-        return iou_val, intersection_vol
+            intersection_vol = intersect_bbox_with_yaw(self.get_bbox(), other.get_bbox())
+            metric_val = metric_fn(self.get_vol_box(), other.get_vol_box(), intersection_vol)
+        return metric_val, intersection_vol
 
     def __repr__(self):
         return str(self.tracklet_idx) + ' ' + str(self.object_type)
+
+
+def get_vol_method(object_type, override=''):
+    if override and override in VOLUME_METHODS:
+        return override
+    if object_type == 'Pedestrian':
+        return 'cylinder'
+    else:
+        return 'bbox'
 
 
 def generate_obstacles(tracklets, override_size=None):
@@ -154,8 +232,8 @@ class EvalFrame(object):
         self.gt_obs = []
         self.pred_obs = []
 
-    def score(self, intersection_count, union_count, pr_at_ious, method='box'):
-        # Perform IOU calculations between all gt and predicted obstacle pairings and greedily match those
+    def score(self, intersection_count, vol_count, pr_at_thresh, method_override='', metric_fn=iou):
+        # Perform IOU/Dice calculations between all gt and predicted obstacle pairings and greedily match those
         # with the largest IOU. Possibly other matching algorithms will work better/be more efficient.
         # NOTE: This is not a tracking oriented matching like MOTA, predicted -> gt affinity context
         # would need to be passed between frame evaluations for that.
@@ -164,49 +242,52 @@ class EvalFrame(object):
         fn = set(range(len(self.gt_obs)))  # gt idx for gt that don't have any intersecting pred
         fp = set(range(len(self.pred_obs)))  # pred idx for pred not intersecting any gt
 
-        # Compute IOU between all obstacle gt <-> prediction pairing possibilities (of same obj type)
+        # Compute metric between all obstacle gt <-> prediction pairing possibilities (of same obj type)
         for p_idx, p in enumerate(self.pred_obs):
             for g_idx, g in enumerate(self.gt_obs):
                 if p.object_type == g.object_type:
-                    iou_val, intersection_vol = g.intersection(p, method=method)
-                    if iou_val > 0:
-                        intersections.append((iou_val, intersection_vol, p_idx, g_idx))
+                    method = get_vol_method(p.object_type, method_override)
+                    metric_val, intersection_vol = g.intersection_metric(p, method=method, metric_fn=metric_fn)
+                    if metric_val > 0:
+                        intersections.append((metric_val, intersection_vol, p_idx, g_idx))
 
         # Traverse calculated intersections, greedily consume intersections with largest overlap first,
         # summing volumes and marking TP/FP/FN at specific IOU thresholds as we go.
         intersections.sort(key=lambda x: x[0], reverse=True)
-        for iou_val, intersection_vol, p_idx, g_idx in intersections:
+        for metric_val, intersection_vol, p_idx, g_idx in intersections:
             if g_idx in fn and p_idx in fp:
                 fn.remove(g_idx)  # consume the ground truth
                 fp.remove(p_idx)  # consume the prediction
-                obs = self.gt_obs[g_idx]
-                #print('IOU: ', iou_val, intersection_vol)
-                intersection_count[obs.object_type] += intersection_vol
-                union_count[obs.object_type] += \
-                    obs.get_vol(method) + self.pred_obs[p_idx].get_vol(method) - intersection_vol
-                for iou_threshold in pr_at_ious.keys():
-                    if iou_val > iou_threshold:
-                        pr_at_ious[iou_threshold]['TP'] += 1
+                gt_obs, pred_obs = self.gt_obs[g_idx], self.pred_obs[p_idx]
+                #print('Metric: ', metric_val, intersection_vol)
+                intersection_count[gt_obs.object_type] += intersection_vol
+                method = get_vol_method(gt_obs.object_type, method_override)
+                vol_count[gt_obs.object_type] += gt_obs.get_vol(method) + pred_obs.get_vol(method)
+                for metric_threshold in pr_at_thresh.keys():
+                    if metric_val > metric_threshold:
+                        pr_at_thresh[metric_threshold]['TP'] += 1
                     else:
                         # It's already determined at this point that this is the highest IOU score
                         # there is for this match and another match for these two bbox won't be considered.
                         # Thus both the gt and prediction should be considered unmatched.
-                        pr_at_ious[iou_threshold]['FP'] += 1
-                        pr_at_ious[iou_threshold]['FN'] += 1
+                        pr_at_thresh[metric_threshold]['FP'] += 1
+                        pr_at_thresh[metric_threshold]['FN'] += 1
 
         # sum remaining false negative volume (unmatched ground truth box volume)
         for g_idx in fn:
-            obs = self.gt_obs[g_idx]
-            union_count[obs.object_type] += obs.get_vol(method)
-            for iou_threshold in pr_at_ious.keys():
-                pr_at_ious[iou_threshold]['FN'] += 1
+            gt_obs = self.gt_obs[g_idx]
+            method = get_vol_method(gt_obs.object_type, method_override)
+            vol_count[gt_obs.object_type] += gt_obs.get_vol(method)
+            for metric_threshold in pr_at_thresh.keys():
+                pr_at_thresh[metric_threshold]['FN'] += 1
 
         # sum remaining false positive volume (unmatched prediction volume)
         for p_idx in fp:
-            obs = self.pred_obs[p_idx]
-            union_count[obs.object_type] += obs.get_vol(method)
-            for iou_threshold in pr_at_ious.keys():
-                pr_at_ious[iou_threshold]['FP'] += 1
+            pred_obs = self.pred_obs[p_idx]
+            method = get_vol_method(pred_obs.object_type, method_override)
+            vol_count[pred_obs.object_type] += pred_obs.get_vol(method)
+            for metric_threshold in pr_at_thresh.keys():
+                pr_at_thresh[metric_threshold]['FP'] += 1
 
 
 def load_indices(indices_file):
@@ -232,8 +313,11 @@ def main():
         help='CSV file containing frame indices to exclude (takes priority over inclusions) from evaluation.')
     parser.add_argument('-o', '--outdir', type=str, nargs='?', default=None,
         help='Output folder')
-    parser.add_argument('-m', '--method', type=str, nargs='?', default='box',
-        help='Volume intersection calculation method. "box" or "sphere" (default = "box")')
+    parser.add_argument('-m', '--method', type=str, nargs='?', default='',
+        help='Volume intersection calculation method override. "box", "cylinder", '
+             '"sphere" (default = "", no override)')
+    parser.add_argument('-v', '--eval_metric', type=str, nargs='?', default='iou',
+        help='Eval metric. "iou" or "dice" (default = "iou")')
     parser.add_argument('-g', dest='override_lwh_with_gt', action='store_true',
         help='Override predicted lwh values with value from first gt tracklet.')
     parser.add_argument('-d', dest='debug', action='store_true', help='Debug print enable')
@@ -244,12 +328,15 @@ def main():
     exclude_indices_file = args.exclude_indices
     output_dir = args.outdir
     override_lwh_with_gt = args.override_lwh_with_gt
-
-    volume_method = args.method
-    if volume_method not in VOLUME_METHODS:
-        sys.stderr.write('Error: Invalid volume method argument "%s". Must be one of %s\n'
-                         % (volume_method, VOLUME_METHODS))
-        exit(-1)
+    eval_metric = args.eval_metric
+    override_volume_method = args.method
+    if override_volume_method:
+        if override_volume_method not in VOLUME_METHODS:
+            print('Warning: Invalid volume method override "%s". Must be one of %s\n'
+                  % (override_volume_method, VOLUME_METHODS))
+            override_volume_method = ''
+        else:
+            print('Overriding volume intersection method with %s' % override_volume_method)
 
     pred_file = args.prediction
     if not os.path.exists(pred_file):
@@ -281,9 +368,9 @@ def main():
         # FIXME START TEST HACK
         if False:
             trans_noise = np.random.normal(0, 0.3, pred_tracklet.trans.shape)
-            #rots_noise = np.random.normal(0, 0.1, pred_tracklet.rots.shape)
+            rots_noise = np.random.normal(0, 0.39, pred_tracklet.rots.shape)
             pred_tracklets[p_idx].trans = pred_tracklet.trans + trans_noise
-            #pred_tracklets[p_idx].rots = pred_tracklet.rots + rots_noise
+            pred_tracklets[p_idx].rots = pred_tracklet.rots + rots_noise
             pred_tracklets[p_idx].size = pred_tracklet.size + np.random.normal(0, 0.2, pred_tracklet.size.shape)
         # FIXME END HACK
 
@@ -335,53 +422,62 @@ def main():
     print('%d ground truth object instances included in evaluation, % s excluded' % (included_gt, excluded_gt))
     print('%d predicted object instances included in evaluation, % s excluded' % (included_pred, excluded_pred))
 
-    iou_thresholds = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
-    pr_at_ious = {k: Counter() for k in iou_thresholds}
+    metric_thresholds = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
+    pr_at_thresh = {k: Counter() for k in metric_thresholds}
+
+    if eval_metric == 'dice':
+        metric_fn = dice
+        METRIC_PER_OBJ = 'dice_score_per_obj'
+        PR_PER_METRIC = 'pr_per_dice_score'
+    else:
+        metric_fn = iou
+        METRIC_PER_OBJ = 'iou_per_obj'
+        PR_PER_METRIC = 'pr_per_iou'
 
     intersection_count = Counter()
-    union_count = Counter()
+    volume_count = Counter()
     for frame_idx in eval_indices:
         #  calc scores
         eval_frames[frame_idx].score(
             intersection_count,
-            union_count,
-            pr_at_ious,
-            method=volume_method)
+            volume_count,
+            pr_at_thresh,
+            method_override=override_volume_method,
+            metric_fn=metric_fn)
 
-    results_table = {'iou_per_obj': {}, 'pr_per_iou': {}}
+    results_table = {METRIC_PER_OBJ: {}, PR_PER_METRIC: {}}
 
     # FIXME determine how we want to combined IOU scores between object classes:
     # - combine volumes across all classes before ratio (vehicles dominate pedestrians)
     # - simple mean of per class ratio (current)
     # - weighted mean of per class ratio
-    iou_sum = 0.0
+    metric_sum = 0.0
     for k in intersection_count.keys():
-        iou = intersection_count[k] / union_count[k] if union_count[k] else 0.
-        results_table['iou_per_obj'][k] = float(iou)
-        iou_sum += iou
-    results_table['iou_per_obj']['All'] = \
-        float(iou_sum / len(intersection_count)) if len(intersection_count) else 0.
+        metric_val = metric_fn(volume_count[k], 0., intersection_count[k])
+        results_table[METRIC_PER_OBJ][k] = float(metric_val)
+        metric_sum += metric_val
+    results_table[METRIC_PER_OBJ]['All'] = \
+        float(metric_sum / len(intersection_count)) if len(intersection_count) else 0.
 
     # FIXME add support for per class P/R scores?
     # NOTE P/R scores need further analysis given their use with the greedy pred - gt matching
-    for k, v in pr_at_ious.items():
+    for k, v in pr_at_thresh.items():
         p = v['TP'] / (v['TP'] + v['FP']) if v['TP'] else 0.0
         r = v['TP'] / (v['TP'] + v['FN']) if v['TP'] else 0.0
-        # f1 = 2 * (p * r) / (p + r) if (p + r != 0) else 0.0
-        results_table['pr_per_iou'][k] = {'precision': p, 'recall': r}
+        results_table[PR_PER_METRIC][k] = {'precision': p, 'recall': r}
 
     print('\nResults')
     print(yaml.safe_dump(results_table, default_flow_style=False, explicit_start=True))
 
     if output_dir is not None:
-        with open(os.path.join(output_dir, 'iou_per_obj.csv'), 'w') as f:
+        with open(os.path.join(output_dir, METRIC_PER_OBJ + '.csv'), 'w') as f:
             f.write('object_type,iou\n')
             [f.write('{0},{1}\n'.format(k, v))
-             for k, v in sorted(results_table['iou_per_obj'].items(), key=lambda x: x[0])]
-        with open(os.path.join(output_dir, 'pr_per_iou.csv'), 'w') as f:
+             for k, v in sorted(results_table[METRIC_PER_OBJ].items(), key=lambda x: x[0])]
+        with open(os.path.join(output_dir, PR_PER_METRIC + '.csv'), 'w') as f:
             f.write('iou_threshold,p,r\n')
             [f.write('{0},{1},{2}\n'.format(k, v['precision'], v['recall']))
-             for k, v in sorted(results_table['pr_per_iou'].items(), key=lambda x: x[0])]
+             for k, v in sorted(results_table[PR_PER_METRIC].items(), key=lambda x: x[0])]
 
 
 if __name__ == '__main__':
